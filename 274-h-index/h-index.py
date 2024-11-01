71b9d6c9b6aaf7213977e1f2090f9e0f1410dd25
Manchov <manchov@outlook.com>
2024-11-01 16:30:23 +0100

    Time: 2 ms (31.52%) | Memory: 17 MB (34.27%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -3,10 +3,9 @@ class Solution:
         citations.sort(reverse=True)
         nums = 0
         for cit in citations:
-            if cit:
-                if cit<nums:
-                    return nums
-                nums+=1
-                if nums>=cit and cit:
-                    return cit
+            if cit <= nums:
+                return nums
+            nums += 1
+            if nums >= cit and cit:
+                return cit
         return nums
